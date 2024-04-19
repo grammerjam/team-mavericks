e4f93ebc55380804290f4c98f0af433b25a5ba4b
josewebdev2000 <josewebdev@proton.me>
2024-04-19 17:33:58 -0400

    Rudimentary Added Theme to Project Using CSS

--- P1/Project_Code/frontend/src/routes/Home/Home.route.jsx
+++ P1/Project_Code/frontend/src/routes/Home/Home.route.jsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 
+const homeStyles = {
+  button: {
+    backgroundColor: "rgb(252, 71, 71)",
+    fontFamily: "Outfit",
+    fontSize: "13px",
+    fontWeight: "300"
+
+  }
+};
+
 function Home() {
   return (
-    <div>
-        <Button variant="contained">Hello world</Button>
+    <div className="page-container">
+        <Button variant="contained" className="body-S" style={homeStyles.button}>Hello world</Button>
     </div>
   );
 }
