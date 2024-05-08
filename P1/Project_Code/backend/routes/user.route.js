373a36255a3847b4318848fd39504753cf419b2c
josewebdev2000 <josewebdev@proton.me>
2024-05-07 20:44:39 -0400

    Fixed URLs in User ROute

--- P1/Project_Code/backend/routes/user.route.js
+++ P1/Project_Code/backend/routes/user.route.js
@@ -10,13 +10,13 @@ const router = express.Router();
 router.post("/register", userController.register);
 // Try out GET http://localhost:8000/user/getAllUsers
 router.get("/getAllUsers", userController.getAllUsers);
-// Try out GET http://localhost:8000/user/<id value>
+// Try out GET http://localhost:8000/user/getUserById/<id value>
 router.get("/getUserById/:id", userController.getUserById);
-// Try out GET http://localhost:8000/user/<username value>
+// Try out GET http://localhost:8000/user/getUserByUsername/<username value>
 router.get("/getUserByUsername/:username", userController.getUserByUsername);
-// Try out GET http://localhost:8000/user/<email value>
+// Try out GET http://localhost:8000/user/getUserByEmail/<email value>
 router.get("/getUserByEmail/:email", userController.getUserByEmail);
-// Try out PUT http://localhost:8000/user/<id value>
+// Try out PUT http://localhost:8000/user/updateUserById/
 router.put("/updateUserById", userController.updateUserById);
 // Try out DELETE http://localhost:8000/user/deleteUserById
 router.delete("/deleteUserById", userController.deleteUserById);
