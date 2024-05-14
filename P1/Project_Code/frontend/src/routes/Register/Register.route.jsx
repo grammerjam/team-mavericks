0e23b2e9031c5936b74985b4cbe309655129d926
gcode101 <gcode101@users.noreply.github.com>
2024-05-14 01:17:52 -0400

    added the login/sigup pages using an mui theme

--- P1/Project_Code/frontend/src/routes/Register/Register.route.jsx
+++ P1/Project_Code/frontend/src/routes/Register/Register.route.jsx
@@ -1,12 +1,68 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
+import { useState } from 'react';
+import { Link } from "react-router-dom";
+import {Button, TextField, FormControl, Container, ThemeProvider, Typography} from '@mui/material';
+import logo from "../../assets/logo.svg";
+import theme from "../Theme.styles";
 
 function Register() {
-  return (
-    <div>
-        <Button variant="contained">Register Page</Button>
-    </div>
-  );
-}
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [confirmPass, setConfirmPass] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log("handleSubmit clicked");
+	}
+
+	return (
+	    <div>
+	    	<ThemeProvider theme={theme}>
+		    	<div className="logo-container">
+		    		<img src={logo} alt="logo img"/>
+		    	</div>
+		    	<Container maxWidth="sm">
+			    	<form onSubmit={handleSubmit}>
+			    		<Typography variant="h2">Sign Up</Typography>
+			    		<TextField
+			    			label="Email address"
+			    			required
+			    			variant="standard"
+			    			sx={{mb: 3}}
+			    			color="secondary"
+			    			type="emial"
+			    			fullWidth
+			    			value={email}
+			    		/>
+			    		<TextField
+			    			label="Password"
+			    			required
+			    			variant="standard"
+			    			color="secondary"
+			    			type="password"
+			    			sx={{mb: 3}}
+			    			fullWidth
+			    			value={password}
+			    		/>
+			    		<TextField
+			    			label="Confirm password"
+			    			required
+			    			variant="standard"
+			    			color="secondary"
+			    			type="password"
+			    			sx={{mb: 3}}
+			    			fullWidth
+			    			value={confirmPass}
+			    		/>
+			    		<Button variant="contained" fullWidth >Login to your account</Button>
+			    	</form>
+			    	<div className="redirect-link">
+			    		<small>Already have an account? <Link to="/login" className="link-font">Login</Link></small>
+			    	</div>
+		    	</Container>
+	    	</ThemeProvider>
+	    </div>
+	  );
+	}
 
 export default Register;
---
+++ P1/Project_Code/frontend/src/routes/Theme.styles.jsx
@@ -0,0 +1,90 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+	typography: {
+		h2: {
+			paddingTop: "40px",
+			paddingBottom: "40px",
+			fontFamily: "Outfit",
+			fontWeight: "300",
+			fontSize: "32px",
+			letterSpacing: "-0.5px"	
+		}
+	},
+	components: {
+		//Custom Override for textfields
+		MuiTextField: {
+			styleOverrides: {
+				root:{
+					"& .MuiInput-root": {
+						color: "#FFFFFF",
+						//Bottom border
+						"&:before":{
+							borderColor: "#FFFFFF",
+							opacity: "0.5"
+						},
+						"&:after": {
+							borderColor: "secondary.main",
+							borderWidth: "3px"
+						},
+						":hover:not(.Mui-focused)":{
+							"&:before":{
+								borderColor: "#e7e7e7",
+								borderWidth: "2px"
+							}
+						}
+					},
+					//Label
+					"& .MuiInputLabel-standard":{
+						color: "#FFFFFF",
+						fontFamily: "Outfit",
+						opacity: "0.5",
+						fontSize: "15px",
+						marginLeft: "15px",
+
+					}
+				}
+			}
+		},
+		//Custom Override for container
+		MuiContainer: {
+			styleOverrides: {
+				root: {
+					backgroundColor: "#161D2F",
+					borderRadius: "20px",
+					marginTop: "20px",
+					color: "#FFFFFF",
+					paddingBottom: "20px"
+				}
+			}
+		},
+		//Custom Override for buttons
+		MuiButton: {
+			styleOverrides: {
+				root: {
+					backgroundColor: "rgb(252, 71, 71)",
+					fontFamily: "Outfit",
+					fontSize: "13px",
+					fontWeight: "300",
+					marginTop: "20px",
+					marginBottom: "20px",
+					padding: "10px",
+
+					"&:hover":{
+						backgroundColor: 'rgba(252, 71, 71, 0.8)', 
+					}  
+				}
+			}
+		},
+	}
+});
+
+export default theme;
+
+
+
+
+
+
+
+
