14969c4c7469e9129c2ca149464dc62041c3763c
ethanfreire <element55star@gmail.com>
2024-05-17 16:00:37 -0400

    updated side nav bar styling, but still need more work to match figma design

--- P1/Project_Code/frontend/src/components/Navigation/NavigationBar.jsx
+++ P1/Project_Code/frontend/src/components/Navigation/NavigationBar.jsx
@@ -1,24 +1,75 @@
-import {Link} from 'react-router-dom';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import LocalMoviesIcon from '@mui/icons-material/LocalMovies';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import MovieIcon from '@mui/icons-material/Movie';
-import TvIcon from '@mui/icons-material/Tv';
-import GridViewIcon from '@mui/icons-material/GridView';
+import { Link } from "react-router-dom";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LocalMoviesIcon from "@mui/icons-material/LocalMovies";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import MovieIcon from "@mui/icons-material/Movie";
+import TvIcon from "@mui/icons-material/Tv";
+import GridViewIcon from "@mui/icons-material/GridView";
+import { Drawer } from "@mui/material";
+import React from "react";
 
 export const NavigationBar = () => {
-    return (
-        <div className="NavigationContainer" style={{border: "1px solid white"}}>
-            <MovieIcon  sx={{ color: 'red'}} />
-            <nav>
-                <ul style={{listStyleType: 'none', display: 'flex', justifyContent: 'space-around'}}>
-                    <li><Link to="/"><GridViewIcon sx={{ color: "white" }} /></Link></li>
-                    <li><Link to="/movies"><LocalMoviesIcon sx={{ color: "white" }} /></Link></li>
-                    <li><Link to="/bookmarks"><BookmarkIcon sx={{ color: "white" }} /></Link></li>
-                    <li><Link to="/tv-series"><TvIcon sx={{ color: "white" }}/></Link></li>
-                </ul>
-            </nav>
-            <AccountCircleIcon sx={{ color: 'white'}}></AccountCircleIcon>
+  const drawerWidth = 75;
+
+  return (
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: drawerWidth,
+          boxSizing: "border-box",
+          display: "flex",
+          flexDirection: "column",
+          backgroundColor: "#161D2F",
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <div
+        className="NavigationContainer"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "100%",
+          paddingTop: "1rem",
+          paddingBottom: "1rem",
+        }}
+      >
+        <div className="MovieIconContainer" style={{ marginBottom: "20px" }}>
+          <MovieIcon sx={{ color: "red" }} />
         </div>
-    )
-}
+        <div
+          className="NavigationMenuItemsContainer"
+          style={{
+            flexGrow: 1,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Link to="/" style={{ margin: "20px 0" }}>
+            <GridViewIcon sx={{ color: "#5A698F" }} />
+          </Link>
+          <Link to="/movies" style={{ margin: "20px 0" }}>
+            <LocalMoviesIcon sx={{ color: "#5A698F" }} />
+          </Link>
+          <Link to="/bookmarks" style={{ margin: "20px 0" }}>
+            <BookmarkIcon sx={{ color: "#5A698F" }} />
+          </Link>
+          <Link to="/tv-series" style={{ margin: "20px 0" }}>
+            <TvIcon sx={{ color: "#5A698F" }} />
+          </Link>
+        </div>
+        <div
+          className="ProfileIconContainer"
+          style={{ marginBottom: "20px", paddingTop: "100px" }}
+        >
+          <AccountCircleIcon sx={{ color: "#5A698F" }} />;
+        </div>
+      </div>
+    </Drawer>
+  );
+};
--- P1/Project_Code/frontend/src/components/Search/SearchBar.jsx
+++ P1/Project_Code/frontend/src/components/Search/SearchBar.jsx
@@ -1,50 +1,50 @@
-import React, { useState, useEffect, useContext } from 'react';
-import SearchIcon from '@mui/icons-material/Search';
+import React, { useState, useEffect, useContext } from "react";
+import SearchIcon from "@mui/icons-material/Search";
 
-import { Search, SearchIconWrapper, StyledInputBase } from './SearchBar.styles';
+import { Search, SearchIconWrapper, StyledInputBase } from "./SearchBar.styles";
 
 // Import MoviesRoute Context
-import { MoviesContext } from '../../context/Movies.context';
+import { MoviesContext } from "../../context/Movies.context";
 
 export const SearchBar = () => {
+  // Import MoviesRoute Context
+  const { movies, filteredMovies, setFilteredMovies } =
+    useContext(MoviesContext);
 
-    // Import MoviesRoute Context
-    const { movies, filteredMovies, setFilteredMovies } = useContext(MoviesContext);
+  // Let the user user the search input
+  const [searchInput, setSearchInput] = useState("");
 
-    // Let the user user the search input
-    const [searchInput, setSearchInput] = useState("");
+  const onHandleInputChange = (e) => {
+    // Change value of searchInput to whatever the user wrote right now
+    const inputVal = e.target.value;
+    setSearchInput(inputVal);
+  };
 
-    const onHandleInputChange = (e) => {
-        // Change value of searchInput to whatever the user wrote right now
-        const inputVal = e.target.value;
-        setSearchInput(inputVal);
-    };
+  // Run this effect whenever searchInput changes
+  useEffect(() => {
+    // Filter movies based on the input value
+    const filtered = movies.filter((movie) =>
+      movie.title.toLowerCase().includes(searchInput.toLowerCase()),
+    );
+    setFilteredMovies(filtered);
+  }, [searchInput]);
 
-     // Run this effect whenever searchInput changes
-    useEffect(() => {
-        // Filter movies based on the input value
-        const filtered = movies.filter(movie =>
-            movie.title.toLowerCase().includes(searchInput.toLowerCase())
-        );
-        setFilteredMovies(filtered);
-    }, [searchInput]);
+  // Run this effect whenever filteredMovies changes
+  // DELETE THIS EFFECT ONCE MOVIES ARE SHOWN IN THE MEDIA CARDS
+  useEffect(() => {
+    console.log(filteredMovies);
+  }, [filteredMovies]);
 
-     // Run this effect whenever filteredMovies changes
-     // DELETE THIS EFFECT ONCE MOVIES ARE SHOWN IN THE MEDIA CARDS
-    useEffect(() => {
-        console.log(filteredMovies);
-    }, [filteredMovies]);
-
-    return (
-                    <Search>
-                        <SearchIconWrapper>
-                            <SearchIcon style={{color: "white"}} />
-                        </SearchIconWrapper>
-                        <StyledInputBase
-                            placeholder="SearchRoute for movies of TV series"
-                            inputProps={{ 'aria-label': 'search' }}
-                            onChange={onHandleInputChange}
-                        />
-                    </Search>
-    )
-}
+  return (
+    <Search>
+      <SearchIconWrapper>
+        <SearchIcon style={{ color: "white" }} />
+      </SearchIconWrapper>
+      <StyledInputBase
+        placeholder="Search for movies of TV series"
+        inputProps={{ "aria-label": "search" }}
+        onChange={onHandleInputChange}
+      />
+    </Search>
+  );
+};
