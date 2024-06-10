2cc9ee7f0cc7bb44aedfdeec7e946238b3adb7e6
ethanfreire <element55star@gmail.com>
2024-06-10 12:55:29 -0400

    created layout styles for layout component & create useViewport hook to track screen width

--- P1/Project_Code/frontend/src/components/Layout/Layout.jsx
+++ P1/Project_Code/frontend/src/components/Layout/Layout.jsx
@@ -3,19 +3,20 @@ import { Outlet } from "react-router-dom";
 import { NavigationBar } from "../Navigation/NavigationBar.jsx";
 import { SearchBar } from "../Search/SearchBar.jsx";
 import { Grid } from "@mui/material";
-
+import { NavigationBarGridContainer } from "./Layout.styles.js";
+import useViewport from "../../services/useViewport.jsx";
 export const Layout = () => {
+    const { width } = useViewport();
+
+    const isMobile = width <= 767;
+    const isTablet = width >= 768 && width <= 1439;
+    const isDesktop = width >= 1440;
+
   return (
     <Grid container spacing={0}>
-      <Grid
-        item
-        xs={1}
-        container
-        justifyContent="center"
-        style={{ paddingTop: "1rem" }}
-      >
+      <NavigationBarGridContainer item xs={isMobile || isTablet ? 0 : 1} container justifyContent="center">
         <NavigationBar></NavigationBar>
-      </Grid>
+      </NavigationBarGridContainer>
       <Grid item xs={11}>
         <SearchBar></SearchBar>
         <div style={{ visibility: "hidden", padding: "10px" }}>xd</div>
--- P1/Project_Code/frontend/src/components/Navigation/NavigationBar.styles.js
+++ P1/Project_Code/frontend/src/components/Navigation/NavigationBar.styles.js
@@ -11,13 +11,26 @@ export const NavBarContainer = styled("div")(({ theme }) => ({
   paddingBottom: "1rem",
   background: "#161D2F",
   borderRadius: "20px",
+// Media queries
+  [`@media (min-width: 769px) and (max-width: 1279px)`]: {
+    width: "100%",
+    height: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
 }));
 
 export const NavBarMenuItemsContainer = styled("div")(({ theme }) => ({
-  flexGrow: 1,
+
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
+  // Media queries
+  [`@media (min-width: 769px) and (max-width: 1279px)`]: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
 }));
 
 export const NavBarLink = styled(Link)(({ theme }) => ({
