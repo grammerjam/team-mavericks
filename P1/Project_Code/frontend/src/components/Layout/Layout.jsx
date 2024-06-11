ce212a8d2214a5c20a183b135b52bb770fa5960f
ethanfreire <element55star@gmail.com>
2024-06-11 14:27:52 -0400

    Got rid of using useViewport hook in layout and instead use material UI breakpoints to switch between and horizontal and vertical nav bar.

--- P1/Project_Code/frontend/src/components/Layout/Layout.jsx
+++ P1/Project_Code/frontend/src/components/Layout/Layout.jsx
@@ -4,17 +4,12 @@ import { NavigationBar } from "../Navigation/NavigationBar.jsx";
 import { SearchBar } from "../Search/SearchBar.jsx";
 import { Grid } from "@mui/material";
 import { NavigationBarGridContainer } from "./Layout.styles.js";
-import useViewport from "../../services/useViewport.jsx";
 export const Layout = () => {
-    const { width } = useViewport();
 
-    const isMobile = width <= 767;
-    const isTablet = width >= 768 && width <= 1439;
-    const isDesktop = width >= 1440;
 
   return (
     <Grid container spacing={0}>
-      <NavigationBarGridContainer item xs={isMobile || isTablet ? 0 : 1} container justifyContent="center">
+      <NavigationBarGridContainer item sm={0} md={0} lg={1} container justifyContent="center">
         <NavigationBar></NavigationBar>
       </NavigationBarGridContainer>
       <Grid item xs={11}>
