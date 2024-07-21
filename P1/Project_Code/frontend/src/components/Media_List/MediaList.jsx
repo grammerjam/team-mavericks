bafbca78e6cc07fdf1c1a85d32c67428a8fe0555
gcode101 <gcode101@users.noreply.github.com>
2024-07-21 13:23:00 -0400

    adjusted size of tv series page heading for smaller screens

--- P1/Project_Code/frontend/src/components/Media_List/MediaList.jsx
+++ P1/Project_Code/frontend/src/components/Media_List/MediaList.jsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect } from "react";
 import { MoviesContext } from "../../context/Movies.context";
-import { Grid, ThemeProvider, Typography } from "@mui/material";
+import { Grid, ThemeProvider, Typography, Box } from "@mui/material";
 import MediaCard from "../Media_Cards/MediaCard";
 import theme from "../../Theme.styles";
 
@@ -23,29 +23,27 @@ const MediaList = ({ categoryType, title }) => {
 	}
 
     return (
-    	<ThemeProvider theme={theme}>
-	        <div>
-	            <Typography className="media-heading" style={{color: "white"}} variant="h1">{title}</Typography>
-	            <div className="media-container">
-		            <Grid container spacing= {4}>
-		            	{
-			            	filteredCategory.map((movie, index) => (
-			            		<Grid 
-									item key={index}
-									xs={6}
-									sm={4}
-									md={4}
-									lg={3}
-									xl={2.4}
-								>
-			            			<MediaCard movie={movie} type="recommended"/>
-			            		</Grid>
-			            	))
-		            	}
-		            </Grid>
-	            </div>
-	        </div>
-        </ThemeProvider>
+		<div>
+			<Box color={"white"} className="font-heading-L media-heading">{title}</Box>
+			<div className="media-container">
+				<Grid container spacing= {4}>
+					{
+						filteredCategory.map((movie, index) => (
+							<Grid 
+								item key={index}
+								xs={6}
+								sm={4}
+								md={4}
+								lg={3}
+								xl={2.4}
+							>
+								<MediaCard movie={movie} type="recommended"/>
+							</Grid>
+						))
+					}
+				</Grid>
+			</div>
+		</div>
     )
 }
 
