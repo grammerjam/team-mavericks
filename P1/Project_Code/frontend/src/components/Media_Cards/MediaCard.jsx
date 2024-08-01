5d125eddea57b39366eaf8b431927570a168951f
ethanfreire <element55star@gmail.com>
2024-08-01 09:36:53 -0400

    Added bookmark image to the upper right hand corner of trending media cards

--- P1/Project_Code/frontend/src/components/Media_Cards/MediaCard.jsx
+++ P1/Project_Code/frontend/src/components/Media_Cards/MediaCard.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
 import { getImagePath } from '../../services/getImagePath.js';
+import bookmarkActive from '../../assets/bookmark-active.png';
+import bookmarkInactive from '../../assets/bookmark-inactive.png';
+import {BookmarkImage} from "./MediaCardsContainer.styles.js";
 
 const MediaCard = ({ movie, type }) => {
     const [imgSrc, setImgSrc] = useState('');
@@ -15,30 +18,34 @@ const MediaCard = ({ movie, type }) => {
     }
 
     return (
-        <>
-            {type.toLowerCase() === "trending" ? (
-                <div className="trending-media-card" style={{ backgroundImage: `url(${imgSrc})` }}>
-                    <div className="trending-media-info">
-                        <div className="media-info">
-                            <h4 className="media-info-item">{movie.year} •</h4>
-                            <h4 className="media-info-item">{movie.category} •</h4>
-                            <h4 className="media-info-item">{movie.rating}</h4>
-                        </div>
-                        <div className="media-title">{movie.title}</div>
-                    </div>
-                </div>
-            ) : (
-                <div className='recommended-card'>
-                    <img src={imgSrc} alt={movie.title} />
-                    <div className="media-info">
-                        <h4 className="media-info-item">{movie.year} •</h4>
-                        <h4 className="media-info-item">{movie.category} •</h4>
-                        <h4 className="media-info-item">{movie.rating}</h4>
-                    </div>
-                    <div className="media-title">{movie.title}</div>
-                </div>
-            )}
-        </>
+      <>
+        {type.toLowerCase() === "trending" ? (
+          <div
+            className="trending-media-card"
+            style={{ backgroundImage: `url(${imgSrc})` }}
+          >
+            <div className="trending-media-info">
+              <div className="media-info">
+                <h4 className="media-info-item">{movie.year} •</h4>
+                <h4 className="media-info-item">{movie.category} •</h4>
+                <h4 className="media-info-item">{movie.rating}</h4>
+              </div>
+              <div className="media-title">{movie.title}</div>
+            </div>
+            <BookmarkImage src={bookmarkActive} ></BookmarkImage>
+          </div>
+        ) : (
+          <div className="recommended-card">
+            <img src={imgSrc} alt={movie.title} />
+            <div className="media-info">
+              <h4 className="media-info-item">{movie.year} •</h4>
+              <h4 className="media-info-item">{movie.category} •</h4>
+              <h4 className="media-info-item">{movie.rating}</h4>
+            </div>
+            <div className="media-title">{movie.title}</div>
+          </div>
+        )}
+      </>
     );
 };
 
--- P1/Project_Code/frontend/src/components/Media_Cards/MediaCardsContainer.styles.js
+++ P1/Project_Code/frontend/src/components/Media_Cards/MediaCardsContainer.styles.js
@@ -1,3 +1,9 @@
 import {styled} from "@mui/material/styles";
 
-// export const TrendingCardContainer =
+export const BookmarkImage = styled("img")`
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    width: 32px;
+    height: 32px;
+`
