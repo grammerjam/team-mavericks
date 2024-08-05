a54e39986f18606e1b2db8a901c206f4fcf8d670
ethanfreire <element55star@gmail.com>
2024-08-04 21:03:39 -0400

    added bookmark icon to trending and recommended cards and implement onClick for the bookmark icon to toggle between active and inactive states

--- P1/Project_Code/frontend/src/components/Media_Cards/MediaCard.jsx
+++ P1/Project_Code/frontend/src/components/Media_Cards/MediaCard.jsx
@@ -2,10 +2,15 @@ import {useState, useEffect, Fragment} from 'react';
 import { getImagePath } from '../../services/getImagePath.js';
 import bookmarkActive from '../../assets/bookmark-active.png';
 import bookmarkInactive from '../../assets/bookmark-inactive.png';
-import {BookmarkImage} from "./MediaCardsContainer.styles.js";
+import {BookmarkImage} from "./MediaCard.styles.jsx";
 
 const MediaCard = ({ movie, type }) => {
     const [imgSrc, setImgSrc] = useState('');
+    const [isBookmarked, setIsBookmarked] = useState(false);
+
+    const handleBookmarkToggle = () => {
+        setIsBookmarked(!isBookmarked);
+    };
 
     // Get the static path to the image required for the media card
     useEffect(() => {
@@ -32,19 +37,25 @@ const MediaCard = ({ movie, type }) => {
               </div>
               <div className="media-title">{movie.title}</div>
             </div>
-            <BookmarkImage src={bookmarkActive}></BookmarkImage>
+            <BookmarkImage src={isBookmarked ? bookmarkActive : bookmarkInactive} onClick={handleBookmarkToggle}
+            ></BookmarkImage>
           </div>
         ) : (
-          <div className="recommended-card">
-            <img src={imgSrc} alt={movie.title} />
-            <img src={bookmarkActive} className="bookmark-icon"/>
-            <div className="media-info">
-              <h4 className="media-info-item">{movie.year} •</h4>
-              <h4 className="media-info-item">{movie.category} •</h4>
-              <h4 className="media-info-item">{movie.rating}</h4>
+            <div className="recommended-card">
+                <img src={imgSrc} alt={movie.title}/>
+                <img
+                    src={isBookmarked ? bookmarkActive : bookmarkInactive}
+                    className="bookmark-icon"
+                    alt="Bookmark Icon"
+                    onClick={handleBookmarkToggle}
+                />
+                <div className="media-info">
+                    <h4 className="media-info-item">{movie.year} •</h4>
+                    <h4 className="media-info-item">{movie.category} •</h4>
+                    <h4 className="media-info-item">{movie.rating}</h4>
+                </div>
+                <div className="media-title">{movie.title}</div>
             </div>
-            <div className="media-title">{movie.title}</div>
-          </div>
         )}
       </>
     );
---
+++ P1/Project_Code/frontend/src/components/Media_Cards/MediaCard.styles.jsx
@@ -0,0 +1,9 @@
+import {styled} from "@mui/material/styles";
+
+export const BookmarkImage = styled("img")`
+    position: absolute;
+    top: 15px;
+    right: 15px;
+    width: 32px;
+    height: 32px;
+`
