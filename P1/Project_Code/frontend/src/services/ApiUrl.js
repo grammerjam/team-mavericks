c581ee73c3c55d9f36a00900de29643842488f78
ethanfreire <element55star@gmail.com>
2024-08-06 14:22:24 -0400

    update origin links for cors in both frontend and backend

--- P1/Project_Code/frontend/src/services/ApiUrl.js
+++ P1/Project_Code/frontend/src/services/ApiUrl.js
@@ -1,5 +1,5 @@
 
 export const getApiUrl = () => {
-	//return "https://vlueberry-api.onrender.com";
-	return "http://localhost:8000";
+	return "https://vlueberry-api.onrender.com";
+	//return "http://localhost:8000";
 }
