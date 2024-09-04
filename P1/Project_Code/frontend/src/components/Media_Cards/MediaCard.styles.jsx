20150bed0cc62feea7c031d1ffb892f451749ce0
gcode101 <gcode101@users.noreply.github.com>
2024-09-04 18:57:02 -0400

    added the play button on hover functionality to trending media cards

--- P1/Project_Code/frontend/src/components/Media_Cards/MediaCard.styles.jsx
+++ P1/Project_Code/frontend/src/components/Media_Cards/MediaCard.styles.jsx
@@ -8,7 +8,7 @@ export const BookmarkImage = styled("img")`
     height: 32px;
 `
 
-export const CardContainer = styled("div")`
+export const TrendingContainer = styled("div")`
     border-radius: 20px;
     width: 470px;
     height: 230px;
@@ -31,12 +31,19 @@ export const CardContainer = styled("div")`
         transition: filter 0.3s ease;
     }
 
-    &:hover::before {
-        filter: brightness(50%);
+    @media (min-width: 769px){
+        &:hover::before {
+            filter: brightness(50%);
+        }
+
+        &:hover .play-button{
+            opacity: 1;
+        }
     }
 
-    &:hover .play-button{
-        opacity: 1;
+    @media (max-width: 767px){
+        width: 240px;
+        height: 140px;
     }
 `
 
@@ -44,8 +51,37 @@ export const PlayButton = styled("div")`
     position: absolute;
     top: 50%;
     left: 50%;
+    width: 117px;
+    height: 48px;
+    border-radius: 28.5px;
     transform: translate(-50%, -50%);
     transition: opacity 0.3s ease;
     opacity: 0;
+    background: #97979770;
+
+    img {
+        position: relative;
+        left: 7.69%;
+        right: 66.67%;
+        top: 18.75%;
+        bottom: 18.75%;
+    }
 `
+export const PlayText = styled("h4")`
+    position: relative;
+    height: 23px;
+    left: 49.57%;
+    right: 20.51%;
+    top: calc(50% - 70px/1 - -0.5px);
+
+    font-family: 'Outfit';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 23px;
+    color: #FFFFFF;
+`
+
+
+
 
--- P1/Project_Code/frontend/src/components/Media_Cards/TrendingMediaCard.jsx
+++ P1/Project_Code/frontend/src/components/Media_Cards/TrendingMediaCard.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { BookmarkImage, CardContainer, PlayButton } from "./MediaCard.styles.jsx";
+import { BookmarkImage, TrendingContainer, PlayButton, PlayText } from "./MediaCard.styles.jsx";
 import bookmarkActive from '../../assets/bookmark-active.png';
 import bookmarkInactive from '../../assets/bookmark-inactive.png';
 import playIcon from '../../assets/icon-play.svg';
 
 const TrendingMediaCard = ({ imgSrc, movie, isBookmarked, toggleBookmark }) => {
     return (
-        <CardContainer imgSrc={imgSrc}>
+        <TrendingContainer imgSrc={imgSrc}>
             <PlayButton className='play-button'>
                 <img src={playIcon} alt='play button'/>
+                <PlayText>Play</PlayText>
             </PlayButton>
             <div className="trending-media-info">
                 <div className="media-info">
@@ -20,7 +21,7 @@ const TrendingMediaCard = ({ imgSrc, movie, isBookmarked, toggleBookmark }) => {
             </div>
             <BookmarkImage src={isBookmarked ? bookmarkActive : bookmarkInactive} onClick={toggleBookmark}
             ></BookmarkImage>
-        </CardContainer>
+        </TrendingContainer>
     );
 };
 export default TrendingMediaCard;
