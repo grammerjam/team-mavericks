86118af13f50eed4be741d8842de4382ad9638cb
ethanfreire <element55star@gmail.com>
2024-09-09 16:01:13 -0400

    updated the tv-series and movie icon to match figma design

--- P1/Project_Code/frontend/src/components/Navigation/NavigationBar.jsx
+++ P1/Project_Code/frontend/src/components/Navigation/NavigationBar.jsx
@@ -1,18 +1,17 @@
 import { useLocation } from "react-router-dom";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import LocalMoviesIcon from "@mui/icons-material/LocalMovies";
-import BookmarkIcon from "@mui/icons-material/Bookmark";
-import MovieIcon from "@mui/icons-material/Movie";
-import TvIcon from "@mui/icons-material/Tv";
-import GridViewIcon from "@mui/icons-material/GridView";
-import { IconButton } from "@mui/material";
 import React, { useState, useContext } from "react";
+import SiteLogoIcon from "@mui/icons-material/Movie";
+import HomeIcon from "../../assets/icon-nav-home.svg";
+import MovieIcon from "../../assets/icon-nav-movies.svg";
+import TvIcon from "../../assets/icon-nav-tv-series.svg";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { IconButton } from "@mui/material";
 import {
   NavBarContainer,
   NavBarMenuItemsContainer,
   NavBarLink,
 } from "./NavigationBar.styles.js";
-
 import { UserContext } from "../../context/User.context.jsx";
 import avatar from "../../assets/image-avatar.png";
 import useViewport from "../../services/useViewport.jsx";
@@ -42,14 +41,22 @@ export const NavigationBar = () => {
   return (
     <NavBarContainer>
       <NavBarLink to="/" onClick={() => handleIconClick("/")}>
-        <MovieIcon sx={{ color: "red", marginBottom: isDesktop ? "20px" : 0 }} />
+        <SiteLogoIcon
+          sx={{ color: "red", marginBottom: isDesktop ? "20px" : 0 }}
+        />
       </NavBarLink>
       <NavBarMenuItemsContainer>
         <NavBarLink to="/" onClick={() => handleIconClick("/")}>
-          <GridViewIcon sx={iconStyle("/")} />
+          <img src={HomeIcon} />
         </NavBarLink>
         <NavBarLink to="/movies" onClick={() => handleIconClick("/movies")}>
-          <LocalMoviesIcon sx={iconStyle("/movies")} />
+          <img src={ MovieIcon} />
+        </NavBarLink>
+        <NavBarLink
+          to="/tv-series"
+          onClick={() => handleIconClick("/tv-series")}
+        >
+          <img src={TvIcon} />
         </NavBarLink>
         <NavBarLink
           to="/bookmarks"
@@ -57,26 +64,29 @@ export const NavigationBar = () => {
         >
           <BookmarkIcon sx={iconStyle("/bookmarks")} />
         </NavBarLink>
-        <NavBarLink
-          to="/tv-series"
-          onClick={() => handleIconClick("/tv-series")}
-        >
-          <TvIcon sx={iconStyle("/tv-series")} />
-        </NavBarLink>
       </NavBarMenuItemsContainer>
-      {
-        user
-        ?
-          (
-            <IconButton aria-label="avatar" onClick={onClickLogoutUser} sx={{  paddingTop: isDesktop ? "6em" : 0 }}>
-                <img src={avatar} alt="User Profile Picture Avatar" width={42} style={{border: "2px solid white", borderRadius: "50%"}} />
-            </IconButton>
-          )
-        :
-          (
-            <AccountCircleIcon sx={{ color: "#5A698F", fontSize: 42, paddingTop: isDesktop ? "4em" : 0 }}  />
-          )
-      }
+      {user ? (
+        <IconButton
+          aria-label="avatar"
+          onClick={onClickLogoutUser}
+          sx={{ paddingTop: isDesktop ? "6em" : 0 }}
+        >
+          <img
+            src={avatar}
+            alt="User Profile Picture Avatar"
+            width={42}
+            style={{ border: "2px solid white", borderRadius: "50%" }}
+          />
+        </IconButton>
+      ) : (
+        <AccountCircleIcon
+          sx={{
+            color: "#5A698F",
+            fontSize: 42,
+            paddingTop: isDesktop ? "4em" : 0,
+          }}
+        />
+      )}
     </NavBarContainer>
   );
 };
