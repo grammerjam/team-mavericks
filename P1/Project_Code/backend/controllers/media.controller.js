a513502a728c8aa2076f4ceac1d6c1a8107e96b5
gcode101 <gcode101@users.noreply.github.com>
2024-11-17 16:05:28 -0500

    added the search media route to backend

--- P1/Project_Code/backend/controllers/media.controller.js
+++ P1/Project_Code/backend/controllers/media.controller.js
@@ -1,4 +1,5 @@
-const { getTrendingMedia } = require("../services/tmdbServices");
+const { getTrendingMedia, searchMedia } = require("../services/tmdbServices");
+const xss = require("xss");
 
 //Fetch trending media
 //@params page: choose page nuber(eg. 1, 2)
@@ -6,11 +7,23 @@ const { getTrendingMedia } = require("../services/tmdbServices");
 exports.fetchTrendingMedia = async (req, res) => {
     const { page, category } = req.query;
     try{
-        const trendingMedia = await getTrendingMedia(page, category);
+        const trendingMedia = await getTrendingMedia(xss(page), xss(category));
         console.log("Trending media fetched successfully");
         res.status(200).json(trendingMedia);
     }catch(error){
         console.error("Error fetching media", error);
         res.status(500).json({error: "Failed to fetch media"});
+    }
+}
+
+exports.searchMediaItems = async (req, res) => {
+    const { query } = req.query;
+    try{
+        const results = await searchMedia(xss(query));
+        console.log("Search successful");
+        res.status(200).json(results);
+    }catch(error){
+        console.error("Error searching for media", error);
+        res.status(500).json({error: "Failed media search"});
     }
 }
