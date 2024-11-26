99e90d51302b569c5edf1155283824020038e289
gcode101 <gcode101@users.noreply.github.com>
2024-11-25 23:36:44 -0500

    added media player functionality to backend

--- P1/Project_Code/backend/routes/media.route.js
+++ P1/Project_Code/backend/routes/media.route.js
@@ -8,6 +8,9 @@ const router = express.Router();
 // Configure HTTP Methods for /media
 // Try out GET http://localhost:8000/media/trending
 router.get("/trending", mediaController.fetchTrendingMedia);
+// Try out GET http://localhost:8000/media/search
 router.get("/search", mediaController.searchMediaItems);
+// Try out GET http://localhost:8000/media/mediaItem
+router.get("/mediaItem", mediaController.fetchMediaItem);
 
 module.exports = router;
