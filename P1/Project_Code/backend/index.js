43e70bab2a0bc23cdf8c891b7b79c4cff281ea69
gcode101 <gcode101@users.noreply.github.com>
2024-12-10 19:35:29 -0500

    changed api url for deployment

--- P1/Project_Code/backend/index.js
+++ P1/Project_Code/backend/index.js
@@ -14,8 +14,8 @@ const app = express();
 
 // Allow CORS
 app.use(cors({
-  // origin: "https://boisterous-cassata-effa74.netlify.app",
-  origin: "http://localhost:5173",
+  origin: "https://boisterous-cassata-effa74.netlify.app",
+  // origin: "http://localhost:5173",
   credentials: true
 }
 ));
